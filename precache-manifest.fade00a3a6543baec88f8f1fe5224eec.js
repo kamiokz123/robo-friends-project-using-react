f14c267afcd2293fc69a9fb03cd78123563cd436
kamiokz123 <kamranhaiderokz888@gmail.com>
2023-03-19 12:41:34 +0500

    Updates

---
+++ precache-manifest.fade00a3a6543baec88f8f1fe5224eec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "da4bead551d5f93b7d89",
+    "url": "/robo-friends-project-using-react/static/css/main.815405a7.chunk.css"
+  },
+  {
+    "revision": "da4bead551d5f93b7d89",
+    "url": "/robo-friends-project-using-react/static/js/main.da4bead5.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/robo-friends-project-using-react/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "aa85ada31bfb80863877",
+    "url": "/robo-friends-project-using-react/static/css/2.03bea8c2.chunk.css"
+  },
+  {
+    "revision": "aa85ada31bfb80863877",
+    "url": "/robo-friends-project-using-react/static/js/2.aa85ada3.chunk.js"
+  },
+  {
+    "revision": "f514bc51a58c60502258",
+    "url": "/robo-friends-project-using-react/static/js/runtime~main.f514bc51.js"
+  },
+  {
+    "revision": "ce7f5c4780bd7641856e4d39534b9b6f",
+    "url": "/robo-friends-project-using-react/index.html"
+  }
+];
